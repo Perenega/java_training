829cbddd2fd6771bae3f93f9554683fd92a11684
Perenega <33180980+Perenega@users.noreply.github.com>
2018-02-05 10:16:11 +0300

    изменены настройки форматирования

--- sandbox/src/main/java/fist.java
+++ sandbox/src/main/java/fist.java
@@ -1,7 +1,7 @@
 public class fist {
 
- public static void main(String[] args) {
-System.out.println("Hello, word!");
-}
+  public static void main(String[] args) {
+    System.out.println("Hello, word!");
+  }
 
 }
